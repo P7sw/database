7af5e1d5cbae581430e17ee05749b80fe4d2d87c
KennethNorholm <70717116+KennethNorholm@users.noreply.github.com>
2020-10-15 08:57:47 +0200

    Added real example text

---
+++ insert-data/02_test_data.sql
@@ -0,0 +1,20 @@
+INSERT INTO posts(postId, title, body, createdBy, category, createdAt, lastModified)
+VALUES (
+1
+, 'I said goodbye to my hair for now'
+, 'I am 27F and I have dealt with with severe depression and anxiety for some time now, but it has been especially bad these last several months.
+
+At the beginning of the year, I shaved my head when my mom lost her hair to chemo so I could do something to physically support her and her journey.
+
+Ever since then, I have been trying to grow it out so I could “fit in” and fit some of the molds of “pretty” in our society. I know every person, no matter their shape, size, or hair style, is beautiful, but you know... societal pressures.
+
+I have been so overwhelmed lately, especially with the mundane tasks of caring for myself. I decided to stop caring about how the world sees me for a second and decided to shave my hair off again.
+
+A literal and figurative weight has been lifted from me again. I don’t have to worry about styling my hair or being self conscious about how it looks in the awkward growing out stages. I can worry about that when my mental health is in better shape.
+
+It feels so good to through societies standards in the wind and do what is life giving to myself for once!'
+, 77777
+, 1622304
+, 01/01/2020
+, 01/01/2020
+)
