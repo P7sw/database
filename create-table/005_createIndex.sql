5da7eed3532411d6b69b41d8c0786ea9405baeb5
KennethNorholm <70717116+KennethNorholm@users.noreply.github.com>
2020-11-02 09:53:54 +0100

    Database changes for indexing

---
+++ create-table/005_createIndex.sql
@@ -0,0 +1,22 @@
+create table index_token (
+ tokenId int not null IDENTITY,
+ token varchar(255)
+ primary key(tokenId)
+);
+
+create table index_postToken(
+ postTokenId int not null IDENTITY,
+ postId int references posts,
+ tokenId int references index_token,
+ primary key(postTokenId),
+ index index_postToken_FK_posts (postId),
+ index index_postToken_FK_index_token (tokenId)
+);
+
+create table index_tokenLocation(
+ tokenLocationId int not null IDENTITY,
+ postTokenId int references index_postToken,
+ location int,
+ primary key(tokenLocationId),
+ index index_tokenLocation_FK_index_post_token (postTokenId)
+);
--- localhost_sql/DeleteAllTables.sql
+++ localhost_sql/DeleteAllTables.sql
@@ -2,3 +2,7 @@ drop table if exists comments;
 drop table if exists posts;
 drop table if exists boards;
 drop table if exists users;
+
+drop table index_tokenLocation;
+drop table index_postToken;
+drop table index_token;
