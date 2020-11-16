6adde8ad5954ba85aa69ace954c6f2e6888b1cf8
Simon Manojlovic <sim465vman@live.dk>
2020-11-16 13:58:07 +0100

    Update 001_createUser.sql

--- create-table/001_createUser.sql
+++ create-table/001_createUser.sql
@@ -3,7 +3,7 @@ create table users(
     name varchar(1000),
     mail varchar(1000),
     password varchar(1000),
-    passwordSalt varchar(1024),
+    passwordSalt varchar(2048),
     PRIMARY KEY (userId),
 );
 
