5b9c56e41e93eefa7cfeddcbc7122eaa549c6e74
KennethNorholm <70717116+KennethNorholm@users.noreply.github.com>
2020-11-18 09:41:15 +0100

    Update 001_createUser.sql

--- create-table/001_createUser.sql
+++ create-table/001_createUser.sql
@@ -3,7 +3,7 @@ create table users(
     name varchar(1000),
     mail varchar(1000) UNIQUE,
     password varchar(1000),
-    passwordSalt varchar(96),
+    passwordSalt varchar(1000),
     PRIMARY KEY (userId),
 );
 
